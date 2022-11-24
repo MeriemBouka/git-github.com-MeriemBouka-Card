c1fd5e1e0535297658f259dbb2bee6c978c854bb
MeriemBouka <meriem.boukaddouss@gmail.com>
2022-11-24 10:28:14 +0100

    footer & icons

--- my-card/src/components/Footer.jsx
+++ my-card/src/components/Footer.jsx
@@ -1,10 +1,17 @@
 import React from "react"
 import "../style/footer.css" 
+import twitter from "../gray-twitter.png" 
+import facebook from "../gray-facebook.png" 
+import instagram from "../gray-instagram.png" 
+import github from "../gray-github.png" 
 
 export default function Footer(){
     return(
         <footer>
-        <h>C'est moi</h>
+        <img className="icon" src={twitter} alt="Twitter icon"/>
+        <img className="icon" src={facebook} alt="Facebook icon"/>
+        <img className="icon" src={instagram} alt="Instagram icon"/>
+        <img className="icon-github" src={github} alt="Github icon"/>
         </footer>
     )
 }
